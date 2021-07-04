963b28111f59501a04b19ea339189b19e9c9516d
杵盐 <im@chuyn.cn>
2021-07-04 14:24:59 +0800

    add short code & prism.js

--- functions.php
+++ functions.php
@@ -15,3 +15,64 @@ add_action( 'wp_enqueue_scripts', 'cashscroll_style' );
  * Your code goes below.
  */
 
+// Enable basic functions for classic editor
+function add_editor_buttons($buttons) {
+    $buttons[] = 'fontselect';
+    $buttons[] = 'fontsizeselect';
+    $buttons[] = 'backcolor';
+    $buttons[] = 'underline';
+    $buttons[] = 'hr';
+    $buttons[] = 'cut';
+    $buttons[] = 'copy';
+    $buttons[] = 'paste';
+    return $buttons;
+}
+add_filter("mce_buttons_2", "add_editor_buttons");
+
+// 页面链接添加 html 后缀
+add_action('init', 'html_page_permalink', -1);
+function html_page_permalink() {
+    global $wp_rewrite;
+    if ( !strpos($wp_rewrite->get_page_permastruct(), '.html')){
+        $wp_rewrite->page_structure = $wp_rewrite->page_structure . '.html';
+    }
+}
+
+// 添加斜杠
+function nice_trailingslashit($string, $type_of_url) {
+    if ( $type_of_url != 'single' && $type_of_url != 'page' )
+      $string = trailingslashit($string);
+    return $string;
+}
+add_filter('user_trailingslashit', 'nice_trailingslashit', 10, 2);
+
+// 代码高亮 Prism.js
+function add_prism() {
+        wp_register_style(
+            'prismCSS', 
+            get_stylesheet_directory_uri() . '/prism.css' //自定义路径
+         );
+          wp_register_script(
+            'prismJS',
+            get_stylesheet_directory_uri() . '/prism.js' //自定义路径
+         );
+        wp_enqueue_style('prismCSS');
+        wp_enqueue_script('prismJS');
+    }
+add_action('wp_enqueue_scripts', 'add_prism');
+
+// Add Shortcode
+function preCode( $atts , $content = null ) {
+
+	// Attributes
+	$atts = shortcode_atts(
+		array(
+			'language' => 'markup',
+		),
+		$atts
+	);
+
+	return '<pre><code class="language:'.$language.'"></code></pre>'
+
+}
+add_shortcode( 'PreCode', 'preCode' );
